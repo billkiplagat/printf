f453e4f4d969f853afe92f9d7576deceec560988
Kook16 <jamesss075@gmail.com>
2023-03-27 05:47:35 +0000

    Update

---
+++ handle_percent.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * handle_percent - prints a %
+ * @count: Number of characters printed
+ */
+void handle_percent(int *count)
+{
+	putchar('%');
+	(*count)++;
+}
---
+++ handle_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * handle_string - prints a string
+ * @ptr: Address of the string
+ * @count: Number of characters printed
+ */
+void handle_string(char *ptr, int *count)
+{
+	while (*ptr)
+	{
+		putchar(*ptr++);
+		(*count)++;
+	}
+}
